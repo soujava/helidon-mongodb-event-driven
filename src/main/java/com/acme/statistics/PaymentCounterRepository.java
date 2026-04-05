6a99f64ba2142ef2f7a2c1c1351e79c0e0672534
Otavio Santana <otaviopolianasantana@gmail.com>
2026-04-05 19:53:52 +0100

    chore: update PaymentCounterRepository to use UUID for id instead of String

--- src/main/java/com/acme/statistics/PaymentCounterRepository.java
+++ src/main/java/com/acme/statistics/PaymentCounterRepository.java
@@ -3,6 +3,8 @@ package com.acme.statistics;
 import jakarta.data.repository.BasicRepository;
 import jakarta.data.repository.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface PaymentCounterRepository extends BasicRepository<PaymentCounter, String> {
+public interface PaymentCounterRepository extends BasicRepository<PaymentCounter, UUID> {
 }
