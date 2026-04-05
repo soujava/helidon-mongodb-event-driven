630767ec1fa3fc35d303b350da3466429e463f0f
Otavio Santana <otaviopolianasantana@gmail.com>
2026-04-05 19:54:16 +0100

    chore: update Product to use UUID for code instead of String

--- src/main/java/com/acme/Product.java
+++ src/main/java/com/acme/Product.java
@@ -6,6 +6,6 @@ import jakarta.nosql.Embeddable;
 import java.util.UUID;
 
 @Embeddable(Embeddable.EmbeddableType.GROUPING)
-public record Product(@Column String code, @Column String name) {
+public record Product(@Column UUID code, @Column String name) {
 
 }
